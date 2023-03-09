7bf94fbb22734412027462d6f489696760b74727
murky kiska <catmasya97@gmail.com>
2023-03-09 22:14:38 +0700

    Добавьте файлы проекта.

---
+++ Task 8.2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Task8 
+{
+    internal class Program
+    {
+        static void PrintDictionary(Dictionary<long, string> dictionary)
+        {
+            Console.WriteLine($"В словаре содержится {dictionary.Count} элементов.");
+            Console.WriteLine("Номер телефона\t\tФИО");
+
+            foreach (var x in dictionary)
+            {
+                Console.WriteLine($"{x.Key}\t\t{x.Value}");
+            }
+        }
+        static void FindInDictionary(Dictionary<long, string> dictionary, long key)
+        {
+            string value;
+
+            if (dictionary.TryGetValue(key, out value))
+            {
+                Console.WriteLine($"Найденная запись: {key}\t\t{value}");
+            }
+            else
+            {
+                Console.WriteLine("Данный номер отсутствует в словаре.");
+            }
+        }
+        static void AddNewPerson(ref Dictionary<long, string> dictionary, long key)
+        {
+            Console.Write("Введите ФИО: ");
+
+            dictionary[key] = Console.Read().ToString();
+        }
+        static void Main(string[] args)
+        {
+            long key;
+
+            Dictionary<long, string> Persons = new Dictionary<long, string>()
+            {
+                [79001001010] = "Иванов Иван Иванович",
+                [79132002020] = "Петров Петр Петрович"
+            };
+
+            while (true)
+            {
+                Console.WriteLine("Продолжайте итеративный ввод номера телефонов и ФИО их владельцев (первым идет ввод номера телефона)");
+                Console.WriteLine("1 - для вывода всего содержимого в консоль");
+                Console.WriteLine("2 - для поиска владельца по номеру телефона");
+                Console.Write("Введите номер телефона или номер режима: ");
+
+                string mode = Console.Read().ToString();
+
+                if (long.TryParse(mode, out key))
+                {
+                    switch (mode)
+                    {
+                        case "1":
+                            PrintDictionary(Persons);
+                            break;
+                        case "2":
+                            FindInDictionary(Persons, key);
+                            break;
+                        default:
+                            if (Persons.ContainsKey(key))
+                            {
+                                Console.WriteLine("Данный номер телефона уже зарегистрирован в сисеме.");
+                            }
+                            else
+                            {
+                                AddNewPerson(ref Persons, key);
+                            }
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Было введено не число!");
+                }
+            }
+        }
+    }
+}
---
+++ Task8/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+
+namespace Task8 
+{
+    internal class Program
+    {
+        // Заполнение листа n случайными числами в диапазоне от 0 до 100
+        static void InitializeList(ref List<int> list, int n)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(rnd.Next(0, 100));
+            }
+        }
+        // Вывод листа в консоль
+        static void PrintList(List<int> list)
+        {
+            Console.WriteLine($"\nЛист содержит {list.Count} элементов.\n");
+            foreach (var x in list) 
+            {
+                Console.WriteLine(x);
+            }
+        }
+        // Удаление чисел, которые больше числа value1, но меньше числа value2
+        static void RemoveItems(ref List<int> list, int value1, int value2)
+        {
+            list.RemoveAll(x => x > value1 && x < value2);
+        }
+        static void Main(string[] args)
+        {
+            // Создайте лист целых чисел. 
+            List<int> List = new List<int>();
+            // Заполните лист 100 случайными числами в диапазоне от 0 до 100.
+            InitializeList(ref List, 100);
+            // Выведите коллекцию чисел на экран.
+            PrintList(List);
+            // Удалите из листа числа, которые больше 25, но меньше 50.
+            RemoveItems(ref List, 25, 50);
+            // Снова выведите числа на экран.
+            PrintList(List);
+        }
+    }
+}
